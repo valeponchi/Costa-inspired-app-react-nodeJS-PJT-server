9aee4f0fb5aae131cca813e3f09ce439bbb8bd3b
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-09-14 12:12:34 +0100

    first commit

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+
+const cors = require('cors')
+
+const app = express()
+
+// ROUTERS
+// const authRouter = require("./resources/auth/router");
+// const loginAuth = require("./middleware/login");
+
+// MIDDLEWARES
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: 'http://localhost:3000', credentials: true })) // Enables the OPTIONS request check in our API
+
+// Auth
+// This is NOT under login protection
+// app.use(authRouter);
+
+// This is your gate keeper to make sure the user is logged in!
+// Any route after this one will be protected by login!
+// app.use(loginAuth);
+
+// ROUTES
+
+app.get('*', (req, res) => {
+	res.status(404).json({ msg: 'No route is matching your request..' })
+})
+
+//CONNECT THE SERVER
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+	console.log(`The server is connected @ http://localhost:${port} ☕`)
+})
